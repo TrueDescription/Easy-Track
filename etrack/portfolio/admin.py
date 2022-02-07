03a48ff1a271a0e247c0543c67239e39031b39b9
TrueDescription <runescape1724@gmail.com>
2022-02-07 12:19:24 -0500

    portfolioPage.html portfolio view is completed. touch ups on view layout also adjusted.

--- etrack/portfolio/admin.py
+++ etrack/portfolio/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Portfolio, Position
+from .models import Portfolio, Position, Transaction
 
 # Register your models here.
 admin.site.register(Portfolio)
 admin.site.register(Position)
+admin.site.register(Transaction)
--- etrack/portfolio/models.py
+++ etrack/portfolio/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+import django.utils.timezone as t
 
 
 # Create your models here.
 
 class Portfolio(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='portfolio', null=True)
     balance = models.IntegerField()
     name = models.CharField(max_length=100, default='Portfolio')
 
@@ -19,8 +20,35 @@ class Position(models.Model):
     name = models.CharField(max_length=100, default='')
     shares = models.IntegerField()
     avg_cost = models.IntegerField()
+    date = models.DateField(default=t.now)
 
     def __str__(self):
         return self.name
 
 
+class Transaction(models.Model):
+    Position = models.ForeignKey(Position, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, default='')
+    ticker = models.CharField(max_length=5)
+    shares = models.IntegerField()
+    avg_cost = models.IntegerField()
+    date = models.DateField(default=t.now)
+
+    def __str__(self):
+        return self.name
+
+
+class Watchlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, default='')
+
+    def __str__(self):
+        return self.name
+
+
+class WatchItem(models.Model):
+    name = models.CharField(max_length=100, default='')
+    ticker = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.name
