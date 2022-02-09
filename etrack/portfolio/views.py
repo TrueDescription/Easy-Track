c16d66ee78f27f7e00e28abc84bea682699f485a
TrueDescription <runescape1724@gmail.com>
2022-02-09 03:41:23 -0500

    delete watch item and portfolio working. Portfolio labels now correctly state the balance of the portfolio in monetary form.

--- etrack/portfolio/views.py
+++ etrack/portfolio/views.py
@@ -1,25 +1,42 @@
+from unicodedata import name
 from django.shortcuts import render, redirect
+from matplotlib import ticker
 from .models import Portfolio, WatchItem
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 
 @login_required(login_url='login')
 def portfolioView(response):
+    total_mv = 0
+    for pf in Portfolio.objects.filter(user=response.user):
+        total_mv += pf.balance
+
+    total_mv = "{:,.2f}".format(total_mv)
+    context = {'total' : total_mv}
+
     if response.method == "POST":
-        print(response.POST)
+        if response.POST.get('deletepf'):
+            Portfolio.objects.filter(name=response.POST.get('deletepf')).delete()
         if response.POST.get('currency'):
-            print(response.POST.get('name'))
             p = Portfolio(name=response.POST.get('name'), balance=0)
             p.save()
             response.user.portfolio.add(p)
             return redirect('http://127.0.0.1:8000/portfolio/')
         else:
-            w = WatchItem(ticker=response.POST.get('name'))
-            w.save()
-            response.user.WatchItem.add(w)
-            return redirect('http://127.0.0.1:8000/portfolio/')
-
-
-    return render(response, 'portfolio/portfolioPage.html', {})
+            if response.POST.get('delete'):
+                WatchItem.objects.filter(ticker=response.POST.get('deletewi')).delete()
+                return redirect('http://127.0.0.1:8000/portfolio/')
+            else:
+                if WatchItem.objects.filter(user=response.user).filter(ticker=response.POST.get('name').upper()).count() > 0:
+                    return redirect('http://127.0.0.1:8000/portfolio/')
+                
+                n = response.POST.get('name').upper()
+                w = WatchItem(ticker=n)
+                w.save()
+                response.user.WatchItem.add(w)
+                return redirect('http://127.0.0.1:8000/portfolio/')
+    
+    return render(response, 'portfolio/portfolioPage.html', context)
